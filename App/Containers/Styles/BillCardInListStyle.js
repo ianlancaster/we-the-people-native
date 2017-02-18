138ed6cc95937ac8abf8d86cea13a6122a406e99
gness1804 <graham.nessler@gmail.com>
2017-02-18 13:57:50 -0700

    Cards are now clearly showing up as cards

--- App/Containers/Styles/BillCardInListStyle.js
+++ App/Containers/Styles/BillCardInListStyle.js
@@ -1,11 +1,9 @@
 const styles = {
   container: {
+    backgroundColor: '#FFF',
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 100,
-    height: 100,
-    backgroundColor: 'rgb(134, 148, 247)',
+    marginBottom: 20,
+    width: 300,
   }
 }
 
