4247d518a37be851449501d3b1109f1ca5e7dab0
Ian Lancaster <ianclancaster@gmail.com>
2017-02-19 15:53:53 -0700

    Add first status indicator bubble in svg

--- App/Components/BillStatus.js
+++ App/Components/BillStatus.js
@@ -1,20 +1,43 @@
 import React, { PropTypes } from 'react'
-import Svg, { Circle } from 'react-native-svg'
+import Svg, {
+    Circle,
+    Ellipse,
+    G,
+    LinearGradient,
+    RadialGradient,
+    Line,
+    Path,
+    Polygon,
+    Polyline,
+    Rect,
+    Symbol,
+    Text,
+    Use,
+    Defs,
+    Stop
+} from 'react-native-svg'
+
+const white = '#ffffff'
+const black = '#000000'
+const green = '#B8E986'
+const yellow = '#FFF1BF'
+const red = '#FFD1D1'
+const lightBlue = '#50739C'
 
 const BillStatus = () => {
   return (
     <Svg
-      height='100'
-      width='100'
+      height='60'
+      width='350'
     >
-      <Circle
-        cx='50'
-        cy='50'
-        r='45'
-        stroke='blue'
-        strokeWidth='2.5'
-        fill='green'
+      <Rect
+        height='60'
+        width='350'
+        strokeWidth='3'
+        fill={white}
+        stroke={black}
       />
+      {TextBubble()}
     </Svg>
   )
 }
@@ -22,6 +45,33 @@ const BillStatus = () => {
 BillStatus.propTypes = {
   exampleProp1: PropTypes.number,
   exampleProp2: PropTypes.number
+}
+
+const TextBubble = () => {
+  return (
+    <G
+      x='20'
+      y='30'
+    >
+      <Circle
+        r='10'
+        fill={green}
+      />
+      <Circle
+        cx='5'
+        r='10'
+        fill={green}
+      />
+      <Text
+        y='-8'
+        x='2'
+        fill={lightBlue}
+        fontSize='12'
+        fontWeight='bold'
+        textAnchor='middle'
+      >HC</Text>
+    </G>
+  )
 }
 
 module.exports = BillStatus
