dbcd5612bc938066397b26ca4bd60200c6f5b784
Ian Lancaster <ianclancaster@gmail.com>
2017-02-19 23:20:52 -0700

    Add additionalData function to bills route on server

--- server/routes/bills.js
+++ server/routes/bills.js
@@ -7,18 +7,25 @@ router.get('/api/bills/:page', (req, res) => {
   fetch(`https://congress.api.sunlightfoundation.com/bills`)
   .then(response => response.json())
   .then(billsAll => billsAll.results.reduce((billsPruned, bill) => {
-    billsPruned.push(pick(bill, [
-      'official_title',
-      'bill_id',
-      'introduced_on',
-      'last_action_at',
-      'chamber',
-      'history'
-    ]))
+    billsPruned.push(Object.assign({},
+      pick(bill, [
+        'official_title',
+        'bill_id',
+        'introduced_on',
+        'last_action_at',
+        'chamber',
+        'history'
+      ]),
+      additionalData()
+    ))
     return billsPruned
   }, []))
   .then(billsPruned => (res.json(billsPruned)))
   .catch(err => res.json(err))
+})
+
+const additionalData = () => ({
+  status: 'active'
 })
 
 module.exports = router
