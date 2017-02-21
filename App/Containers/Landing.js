4b7276e8a5d905f2fc8a0c9b4757f070c1f61392
Ian Lancaster <ianclancaster@gmail.com>
2017-02-20 18:09:43 -0700

    Add all logic for bill status graphic

--- App/Containers/Landing.js
+++ App/Containers/Landing.js
@@ -30,13 +30,30 @@ export default class Landing extends React.Component {
           onPress={this.test}
         />
         <ScrollView>
-          <BillStatus />
-          <BillStatus />
-          <BillStatus />
-          <BillStatus />
-          <BillStatus />
-          <BillStatus />
-          <BillStatus />
+          <BillStatus progress={{ index: 23 }} />
+          <BillStatus progress={{ index: 22 }} />
+          <BillStatus progress={{ index: 21 }} />
+          <BillStatus progress={{ index: 20 }} />
+          <BillStatus progress={{ index: 19 }} />
+          <BillStatus progress={{ index: 18 }} />
+          <BillStatus progress={{ index: 17 }} />
+          <BillStatus progress={{ index: 16 }} />
+          <BillStatus progress={{ index: 15 }} />
+          <BillStatus progress={{ index: 14 }} />
+          <BillStatus progress={{ index: 13 }} />
+          <BillStatus progress={{ index: 12 }} />
+          <BillStatus progress={{ index: 11 }} />
+          <BillStatus progress={{ index: 10 }} />
+          <BillStatus progress={{ index: 9 }} />
+          <BillStatus progress={{ index: 8 }} />
+          <BillStatus progress={{ index: 7 }} />
+          <BillStatus progress={{ index: 6 }} />
+          <BillStatus progress={{ index: 5 }} />
+          <BillStatus progress={{ index: 4 }} />
+          <BillStatus progress={{ index: 3 }} />
+          <BillStatus progress={{ index: 2 }} />
+          <BillStatus progress={{ index: 1 }} />
+          <BillStatus progress={{ index: 0 }} />
         </ScrollView>
       </View>
     )
