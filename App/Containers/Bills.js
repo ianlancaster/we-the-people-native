7389b13ff3aff868402f1b02e7b9733a8be9992d
gness1804 <graham.nessler@gmail.com>
2017-02-22 11:56:52 -0700

    Refactored API call in Bills

--- App/Containers/Bills.js
+++ App/Containers/Bills.js
@@ -1,18 +1,8 @@
 import React from 'react'
 import { Text, View, ScrollView, TouchableOpacity, Button } from 'react-native'
 import styles from './Styles/BillsStyle'
-import APIBills from '../Services/APIBills'
-import FJSON from 'format-json'
-import FullButton from '../Components/FullButton'
 import BillCardInList from './BillCardInList'
-import APIResult from './APIResult'
 import { Actions as NavigationActions } from 'react-native-router-flux'
-
-const endpoints = [
-  { label: 'Get Bills', endpoint: 'getTheBills' },
-  { label: 'pageOne', endpoint: 'getBills', args: [1] },
-  { label: 'pageTwo', endpoint: 'getBills', args: [2] }
-]
 
 export default class Bills extends React.Component {
   constructor (props) {
@@ -22,12 +12,35 @@ export default class Bills extends React.Component {
       showOnlyActive: this.props.showOnlyActive || false,
       sortByDateIntroduced: this.props.sortByDateIntroduced || false
     }
-
-    this.api = APIBills.create()
   }
 
   componentWillMount () {
-    this.tryEndpoint()
+    fetch('http://localhost:3000/api/bills/1', {
+    }).then(res => res.json())
+      .then(bills => {
+        this.mapBills(bills)
+      }).catch(err => { throw new Error(err) })
+  }
+
+  mapBills (bills) {
+    if (this.state.showOnlyActive) {
+      bills = bills.filter((bill) => {
+        return bill.status === 'active'
+      })
+    }
+    if (this.state.sortByDateIntroduced) {
+      bills = bills.sort((a, b) => {
+        return Date.parse(a.introduced_on) - Date.parse(b.introduced_on)
+      })
+    }
+    this.setState({ bills: bills.map((bill) => {
+      return <BillCardInList
+        {...bill}
+        key={bill.bill_id}
+        onChange={this.showDetailedBill}
+        />
+    })
+    })
   }
 
   showAllBills = () => {
@@ -53,43 +66,8 @@ export default class Bills extends React.Component {
     })
   }
 
-  showResult (response, title) {
-    let bills = response.data
-    if (response.ok) {
-      if (this.state.showOnlyActive) {
-        bills = response.data.filter((bill) => {
-          return bill.history.active === true
-        })
-      }
-      if (this.state.sortByDateIntroduced) {
-        bills = response.data.sort((a, b) => {
-          return Date.parse(a.introduced_on) - Date.parse(b.introduced_on)
-        })
-      }
-      this.setState({ bills })
-      this.billData = this.state.bills.map((bill) => {
-        return <BillCardInList
-          {...bill}
-          key={bill.bill_id}
-          onChange={this.showDetailedBill}
-          />
-      })
-    } else {
-      throw new Error('There was a problem with the API call.')
-    }
-  }
-
-  tryEndpoint () {
-    const endpoint = endpoints[0].endpoint
-    this.api[endpoint].apply(this)
-      .then((result) => {
-        this.showResult(result)
-      })
-      .then(() => this.showAllBills())
-  }
-
   render () {
-    const { showOnlyActive, sortByDateIntroduced, title, id, dateIntroduced, lastAction, chamber, sponsor, list } = this.state
+    const { bills, showOnlyActive, sortByDateIntroduced } = this.state
     return (
       <View style={styles.container}>
         <ScrollView style={styles.scrollContainer} ref={() => 'container'}>
@@ -111,7 +89,7 @@ export default class Bills extends React.Component {
             >
             <Text style={styles.showHideBills}>Show Only Active Bills</Text>
           </TouchableOpacity>}
-          {this.billData ? this.billData : <Text>Loading....</Text>}
+          {bills || <Text>Loading....</Text>}
         </ScrollView>
       </View>
     )
