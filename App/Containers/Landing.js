0a5b25adcbaea294edd021253473ee24a1aad4ed
gness1804 <graham.nessler@gmail.com>
2017-02-25 13:50:54 -0700

    Add working bills button on landing

--- App/Containers/Landing.js
+++ App/Containers/Landing.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Text, View, ScrollView, Button, Image } from 'react-native'
+import { Text, View, ScrollView, Button, Image, TouchableOpacity } from 'react-native'
 import styles from './Styles/LandingStyle'
 import { Images } from '../Themes'
 import BillStatus from '../Components/BillStatus'
+import { Actions as NavigationActions } from 'react-native-router-flux'
 
 export default class Landing extends React.Component {
+
+  goToBills = () => {
+    NavigationActions.bills()
+  }
 
   render () {
     return (
@@ -20,6 +25,13 @@ export default class Landing extends React.Component {
           style={styles.bottomImage}
         >
           <Text style={styles.text}>Welcome to We The People! Help keep Congress accountable to the people it serves. View current bills and new laws, and then take action!</Text>
+          <TouchableOpacity
+            onPress={this.goToBills}
+          >
+            <Text>
+              Go to Bills!
+            </Text>
+          </TouchableOpacity>
         </Image>
       </View>
     )
