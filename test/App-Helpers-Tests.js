37b2ee250724c047abaf7861f05119068085c5ce
gness1804 <graham.nessler@gmail.com>
2017-02-28 13:59:57 -0700

    Set up first passing unit test

---
+++ test/App-Helpers-Tests.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import { expect, assert } from 'chai'
+import getRidOfTargetBill from '../App/Helpers/getRidOfTargetBill'
+
+describe('getRidOfTargetBill', function () {
+  it('should be a function', function () {
+    assert.isFunction(getRidOfTargetBill)
+  })
+})
